4c326eb44871abc5f4e3ce4627de9d0f54e2f24e
Manikkumar <manikkumar@Manikkumars-MacBook-Pro.local>
2022-11-28 23:02:13 +0530

    adding ping api

--- be/main.go
+++ be/main.go
@@ -1,8 +1,14 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("hello world")
-}
+	r := gin.Default()
 
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
+}
